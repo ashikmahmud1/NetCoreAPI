66cd2cb9cb3342836172fefc3c85d2acca916f9e
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2020-11-17 19:52:23 +0600

    Added basket functionality

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,7 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-            services.AddSingleton<ConnectionMultiplexer>(c => {
+            services.AddSingleton<IConnectionMultiplexer>(c => {
                 var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
                 return ConnectionMultiplexer.Connect(configuration); 
             });
