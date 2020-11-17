bbb2fa37f9198c94bae653f2de812ad0b50a9dfc
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2020-11-17 17:58:37 +0600

    Enabled cors (cross origin resource sharing)

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,11 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http//localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
